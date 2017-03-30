fa5b9bdd7dab07eda725a9caf901fc0b5b612710
megan liken <meganliken@megans-MacBook-Pro-2.local>
2017-03-29 19:53:21 -0500

    new js files

---
+++ controllers/headline.js
@@ -0,0 +1,7 @@
+// controller for our headlines
+
+//bring in our scrape script and makeDate scripts
+var scrape = require("../scripts/scrape");
+var makeDate = require("../scripts/date");
+
+//bring in the headline and note mongoose models
